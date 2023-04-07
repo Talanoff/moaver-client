edeac9590a2a3f1e9affaf5ae33ddf31f1a8cfd8
Horopchanu <as.khoropchanu@knmu.edu.ua>
2023-04-07 15:26:00 +0300

    delete dark

--- components/modals/transporters/footer-component.vue
+++ components/modals/transporters/footer-component.vue
@@ -2,12 +2,12 @@
   <footer class="flex justify-between items-center mt-8">
     <div class="w-1/2 sm:w-1/4">
       <h2>Step {{ store.currentStep }} van {{ store.steps.length }} </h2>
-      <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+      <div class="w-full bg-gray-200 rounded-full h-2.5">
         <div :style="{width:width }" class="bg-blue-600 h-2.5 rounded-full"></div>
       </div>
     </div>
     <button
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center"
         type="submit">
       <slot/>
     </button>
--- components/transportersForm.vue
+++ components/transportersForm.vue
@@ -16,7 +16,7 @@
               />
             </div>
             <div v-if="step.add" @click="addField(step)"
-                 class="m-2.5 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm   px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                 class="m-2.5 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm   px-5 py-2.5 text-center">
               Add
             </div>
 
