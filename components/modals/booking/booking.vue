ba363d764befad6c392ae582477be608b4515f07
Horopchanu <as.khoropchanu@knmu.edu.ua>
2023-04-07 16:42:05 +0300

    fix position form modal

--- components/modals/booking/booking.vue
+++ components/modals/booking/booking.vue
@@ -1,30 +1,33 @@
 <template>
-  <form class="fixed top-0 w-full" @submit.prevent="submit">
-    <div
-        class="flex bg-gray-600  bg-opacity-50 items-center justify-center absolute h-screen w-screen top-0 left-0"
-        @click="store.booking = false"
-    />
-    <div
-        class="relative lg:w-1/2 sm:w-3/4 w-[90%] m-auto absolute sm:top-28 top-12 left-0 sm:p-10 p-6 bg-white rounded-3xl z-20 max-w-[600px]">
-      <button
-          class="absolute -top-5 -right-5 z-10 flex justify-center items-center px-3 py-3 bg-blue-900 rounded-lg text-gray-100"
-          @click="store.booking = false">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-             class="w-5 h-5">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-        </svg>
-      </button>
-      <div class="border-blue-600 p-2 rounded-md border-2 w-10 cursor-pointer" v-if="store.currentStep > 1"
-           @click="back()">
-        <img
-            src="/icons/arrow-left.png"
-            alt="">
+  <div class="fixed top-0 w-screen h-screen">
+    <form class="flex justify-center items-center w-full z-[999] w-screen h-screen" @submit.prevent="submit">
+      <div
+          class="flex bg-gray-600  bg-opacity-50 items-center justify-center absolute h-screen w-screen top-0 left-0"
+          @click="store.booking = false"
+      />
+      <div
+          class="relative lg:w-1/2 sm:w-3/4 w-[90%] m-auto absolute left-0 sm:p-10 p-6 bg-white rounded-3xl z-20 max-w-[600px]">
+        <button
+            class="absolute -top-5 -right-5 z-10 flex justify-center items-center px-3 py-3 bg-blue-900 rounded-lg text-gray-100"
+            @click="store.booking = false">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+               stroke="currentColor"
+               class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+          </svg>
+        </button>
+        <div class="border-blue-600 p-2 rounded-md border-2 w-10 cursor-pointer" v-if="store.currentStep > 1"
+             @click="back()">
+          <img
+              src="/icons/arrow-left.png"
+              alt="">
+        </div>
+        <client-form class="mb-8" v-if="!store.login && !store.transporters"/>
+        <footer-component>{{ store.steps.length !== store.currentStep ? 'Next' : 'submit' }}</footer-component>
       </div>
-      <client-form class="mb-8" v-if="!store.login && !store.transporters"/>
-      <footer-component>{{ store.steps.length !== store.currentStep ? 'Next' : 'submit' }}</footer-component>
-    </div>
 
-  </form>
+    </form>
+  </div>
 </template>
 
 <script setup>
--- components/modals/login/login.vue
+++ components/modals/login/login.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="fixed top-0 w-full">
+  <div class="fixed top-0 w-screen h-screen">
+    <div class="flex justify-center items-center w-full z-[999] w-screen h-screen">
 
-    <div
-        class="flex bg-gray-600  bg-opacity-50 items-center justify-center absolute h-screen w-screen top-0 left-0"
-        @click="store.login = false"
-    />
-    <div
-        class="relative lg:w-1/2 sm:w-3/4 w-[90%] m-auto absolute sm:top-28 top-12 left-0 p-10 bg-white rounded-3xl z-20 max-w-[600px]">
-      <div>
-        <button
-            class="absolute -top-5 -right-5 z-10 flex justify-center items-center px-3 py-3 bg-blue-900 rounded-lg text-gray-100"
-            @click="store.login = false">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-               stroke="currentColor"
-               class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-          </svg>
-        </button>
+      <div
+          class="flex bg-gray-600  bg-opacity-50 items-center justify-center absolute h-screen w-screen top-0 left-0"
+          @click="store.login = false"
+      />
+      <div
+          class="relative lg:w-1/2 sm:w-3/4 w-[90%] m-auto absolute  left-0 p-10 bg-white rounded-3xl z-20 max-w-[600px]">
+        <div>
+          <button
+              class="absolute -top-5 -right-5 z-10 flex justify-center items-center px-3 py-3 bg-blue-900 rounded-lg text-gray-100"
+              @click="store.login = false">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor"
+                 class="w-5 h-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+            </svg>
+          </button>
+        </div>
+        <form @submit.prevent="submit">
+          <h2 class="text-xl font-bold mt-4 mb-4">
+            Sign in
+          </h2>
+          <form-input class="mb-4" v-model="form.email"
+                      v-bind="{type: 'email', placeholder:'email', required:true, name:'email'}"/>
+          <form-input v-model="form.password"
+                      v-bind="{type: 'password', placeholder:'password', required:true, name:'password'}"/>
+          <footer-component>Sign in
+          </footer-component>
+        </form>
+
       </div>
-      <form @submit.prevent="submit">
-        <h2 class="text-xl font-bold mt-4 mb-4">
-          Sign in
-        </h2>
-        <form-input class="mb-4" v-model="form.email"
-                    v-bind="{type: 'email', placeholder:'email', required:true, name:'email'}"/>
-        <form-input v-model="form.password"
-                    v-bind="{type: 'password', placeholder:'password', required:true, name:'password'}"/>
-        <footer-component>Sign in
-        </footer-component>
-      </form>
 
     </div>
-
   </div>
 </template>
 
