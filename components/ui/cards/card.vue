50bcf27d2c380d4f5d91f7a698698d0eed24df4a
alex.h <alex.h@webcap.com>
2023-04-14 12:30:10 +0300

    feat(localization): translate card

--- components/ui/cards/card.vue
+++ components/ui/cards/card.vue
@@ -24,8 +24,7 @@
                  stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
             </svg>
-
-            {{ category.name }}
+            {{ $t(`categories.${category.id}`) }}
         </div>
     </button>
 </template>
