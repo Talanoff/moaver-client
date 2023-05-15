2a62586cb00e9eeb7dc306bee9a9c6fcc45d8798
Oleh <talanov.o@gmail.com>
2023-05-15 15:15:36 +0300

    update for booking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,28 @@
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
-        '@nuxtjs/i18n',
+        [
+            '@nuxtjs/i18n', {
+            legacy: false,
+            locale: 'en',
+            strategy: 'no_prefix',
+            defaultLocale: 'en',
+            lazy: true,
+            langDir: 'translations',
+            locales: [
+                { code: 'en', file: 'en.json' },
+                { code: 'nl', file: 'nl.json' },
+            ],
+            vueI18n: {
+                fallbackLocale: 'en',
+            },
+            detectBrowserLanguage: {
+                useCookie: true,
+                cookieKey: 'i18n_redirected',
+                redirectOn: 'root',
+            }
+        }
+        ],
         '@vueuse/nuxt',
         '@pinia/nuxt',
         'nuxt-sanctum-auth', // https://github.com/dystcz/nuxt-sanctum-auth
