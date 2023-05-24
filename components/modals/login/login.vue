b8293149aad339099815a6d1272590b4162571b8
Oleh <talanov.o@gmail.com>
2023-05-24 16:53:46 +0300

    styling

--- components/modals/login/login.vue
+++ components/modals/login/login.vue
@@ -1,61 +1,43 @@
 <template>
-    <div class="fixed top-0 w-screen h-screen z-20">
-        <div class="flex justify-center items-center z-[100] w-screen h-screen">
-            <div
-                    class="flex bg-slate-600  bg-opacity-50 items-center justify-center absolute h-screen w-screen top-0 left-0"
-                    @click="authStore.onModalToggle()"
-            />
-            <div class="lg:w-1/2 sm:w-3/4 w-[90%] m-auto absolute inset-x-0 mx-auto p-10 bg-white rounded-3xl z-20 max-w-[600px]">
-                <div>
-                    <button
-                            class="absolute -top-5 -right-5 z-10 flex justify-center items-center px-3 py-3 bg-blue-900 rounded-lg text-slate-100"
-                            @click="authStore.onModalToggle()"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-                        </svg>
-                    </button>
-                </div>
-                <form @submit.prevent="submit">
-                    <h2 class="text-xl font-bold mt-4 mb-4">
-                        {{ $t(`navigation.header.sign_in`) }}
-                    </h2>
-
-                    <div class="mb-4">
-                        <form-input
-                                type="email"
-                                placeholder="example@mail.com"
-                                v-model="form.email"
-                                :required="true"
-                        />
-                    </div>
-
-                    <div>
-                        <form-input
-                                type="password"
-                                v-model="form.password"
-                                placeholder="********"
-                                :required="true"
-                        />
-                    </div>
-
-                    <footer class="mt-8">
-                        <x-button type="submit" theme="primary">
-                            <loader class="w-5 h-5 mr-2.5" v-if="loadingRef"/>
-                            {{ $t('navigation.header.sign_in') }}
-                        </x-button>
-                    </footer>
-                </form>
+    <modal
+        :title="$t(`navigation.header.sign_in`)"
+        @close="authStore.onModalToggle()"
+        container-max-width="tight"
+    >
+        <form @submit.prevent="submit">
+            <div class="mb-4">
+                <form-input
+                    type="email"
+                    placeholder="example@mail.com"
+                    v-model="form.email"
+                    :required="true"
+                />
             </div>
-        </div>
-    </div>
+
+            <div>
+                <form-input
+                    type="password"
+                    v-model="form.password"
+                    placeholder="********"
+                    :required="true"
+                />
+            </div>
+
+            <footer class="mt-8">
+                <x-button type="submit" theme="primary">
+                    <loader class="w-5 h-5 mr-2.5" v-if="loadingRef"/>
+                    {{ $t('navigation.header.sign_in') }}
+                </x-button>
+            </footer>
+        </form>
+    </modal>
 </template>
 
 <script setup>
 import FormInput from "~/components/ui/form-input.vue";
 import XButton from "~/components/ui/x-button.vue";
 import Loader from "~/components/loaders/loader.vue";
+import Modal from "~/components/modals/modal.vue";
 import { useAuth } from "~/store/auth";
 
 const authStore = useAuth();
--- components/modals/modal.vue
+++ components/modals/modal.vue
@@ -5,31 +5,27 @@
                 @click="$emit('close')"
         />
 
-        <div class="z-[100] max-w-[50rem] w-[90vw] m-auto absolute inset-x-0 top-1/2 -translate-y-1/2">
+        <div class="z-[100] w-[90vw] m-auto absolute inset-x-0 top-1/2 -translate-y-1/2" :class="containerWidth">
             <div class="relative sm:p-8 p-5 bg-white rounded-3xl z-10">
                 <button
                         type="button"
                         class="absolute -top-5 -right-5 z-10 flex justify-center items-center p-3 bg-slate-900 rounded-lg text-slate-100"
                         @click="$emit('close')"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor"
-                         class="w-5 h-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
                     </svg>
                 </button>
 
-                <div class="flex items-center space-x-5">
+                <div class="flex items-center space-x-5 mb-10">
                     <div v-if="showBackButton && showTitle">
                         <button
                                 type="button"
                                 class="border-slate-300 hover:border-blue-600 rounded-md border-2 p-2 transition-colors duration-300"
                                 @click.prevent="$emit('back')"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"/>
                             </svg>
                         </button>
                     </div>
@@ -38,13 +34,39 @@
                     </h2>
                 </div>
 
-                <slot/>
+                <section>
+                    <slot/>
+                </section>
             </div>
         </div>
     </div>
 </template>
 
-<script setup>
-defineProps(['title', 'showBackButton', 'showTitle']);
-defineEmits(['back', 'close'])
+<script setup lang="ts">
+const props = defineProps({
+    title: String,
+    showBackButton: {
+        type: Boolean,
+        default: false
+    },
+    showTitle: {
+        type: Boolean,
+        default: true
+    },
+    containerMaxWidth: {
+        type: String,
+        default: 'wide'
+    }
+});
+
+defineEmits(['back', 'close']);
+
+const containerWidth = computed(() => {
+    switch (props.containerMaxWidth) {
+        case 'wide':
+            return 'max-w-[50rem]';
+        case 'tight':
+            return 'max-w-[37.5rem]'
+    }
+})
 </script>
