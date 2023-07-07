dc2e493f0ce42a5d2efa2363140feaffde2a248a
Oleh <talanov.o@gmail.com>
2023-07-07 16:31:12 +0300

    fix images for edge

--- components/ui/responsive-hero-image.vue
+++ components/ui/responsive-hero-image.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
-import useEdge from "~/composables/use-edge";
-
 const props = defineProps<{
   name: string;
 }>()
 
-const isEdge = useEdge();
-
-const extension = ref<string>(isEdge ? 'jpg' : 'avif');
-const srcset = ref<string>(`/images/hero/${props.name}-hero-400w.${extension.value} 400w, /images/hero/${props.name}-hero-600w.${extension.value} 600w, /images/hero/${props.name}-hero-800w.${extension.value} 800w, /images/hero/${props.name}-hero-1000w.${extension.value} 1000w, /images/hero/${props.name}-hero-1200w.${extension.value} 1200w, /images/hero/${props.name}-hero-1600w.${extension.value} 1600w, /images/hero/${props.name}-hero-2000w.${extension.value} 2000w`);
+const srcset = ref<string>(`/images/hero/${props.name}-hero-400w.jpg 400w, /images/hero/${props.name}-hero-600w.jpg 600w, /images/hero/${props.name}-hero-800w.jpg 800w, /images/hero/${props.name}-hero-1000w.jpg 1000w, /images/hero/${props.name}-hero-1200w.jpg 1200w, /images/hero/${props.name}-hero-1600w.jpg 1600w, /images/hero/${props.name}-hero-2000w.jpg 2000w`);
 const src = ref<string>(`/images/hero/${props.name}-hero.png`);
 </script>
 
 <template>
-  <client-only>
-    <nuxt-img
-        sizes="(max-width: 400px) 400px, (max-width: 600px) 600px, (max-width: 800px) 800px, (max-width: 1000px) 1000px, (max-width: 1200px) 1200px, (max-width: 1600px) 1600px, (min-width: 1601px) 2000px"
-        :srcset="srcset"
-        :src="src"
-        class="relative z-0 min-h-full min-w-full object-fit"
-        alt=""
-    />
-  </client-only>
+  <nuxt-img
+      sizes="(max-width: 400px) 400px, (max-width: 600px) 600px, (max-width: 800px) 800px, (max-width: 1000px) 1000px, (max-width: 1200px) 1200px, (max-width: 1600px) 1600px, (min-width: 1601px) 2000px"
+      :srcset="srcset"
+      :src="src"
+      class="relative z-0 min-h-full min-w-full object-fit"
+      alt=""
+  />
 </template>
--- composables/use-edge.ts
+++
@@ -1,7 +0,0 @@
-export default function (): boolean | undefined {
-    if (!process.browser) {
-        return undefined;
-    }
-
-    return /Edge/.test(navigator.userAgent);
-}
